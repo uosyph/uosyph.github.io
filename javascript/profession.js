be3c054fa1a2db91df2bf6c9118ddc6b5f9512d1
Yousef Saeed <107230184+yousafesaeed@users.noreply.github.com>
2022-06-27 23:42:33 +0200

    added some stuff

---
+++ javascript/profession.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const {
+  Splitting
+} = window;
+const results = Splitting();
+const glitches = '`¡™£¢∞§¶•ªº–≠åß∂ƒ©˙∆˚¬…æ≈ç√∫˜µ≤≥÷/?░▒▓<>/'.split('');
+
+for (let r = 0; r < results.length; r++) {
+    const chars = results[r].chars;
+
+    for (let c = 0; c < chars.length; c++) {
+        chars[c].style.setProperty('--count', Math.random() * 5 + 1);
+
+        for (let g = 0; g < 10; g++) {
+        chars[c].style.setProperty(`--char-${g}`, `"${glitches[Math.floor(Math.random() * glitches.length)]}"`);
+        }
+    }
+}
+
+window.ScrollOut({
+    scrollingElement: '.ctr',
+    targets: 'h3'
+});
