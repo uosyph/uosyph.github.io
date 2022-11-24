ae875d2af9334c749b46d4ffa9c5963b5e22632a
Yousef Saeed <107230184+yousafesaeed@users.noreply.github.com>
2022-11-24 06:59:45 +0200

    sass & JS opts

---
+++ javascript/index/title.js
@@ -0,0 +1,27 @@
+import { TextScramble } from '../utils/title.js'
+
+const phrases = [
+    'Full-Stack Developer'
+    // 'What are you looking for?',
+    // 'There is nothing to see here'
+]
+
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+    // generating number in (x-y range) to reapet the animation
+    function generateRandomIntegerInRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let rndmNum = generateRandomIntegerInRange(8000, 16000);
+
+    fx.setText(phrases[counter]).then(() => {
+        // frequency
+        setTimeout(next, rndmNum)
+    })
+    counter = (counter + 1) % phrases.length
+}
+
+next()
