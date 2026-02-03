db716f075b29375b28221221b3d9c760dd16b451
Yousef Saeed <uosyph@gmail.com>
2026-02-03 22:29:33 +0200

    Update portfolio

--- js/utils/colorRandomizer.js
+++ js/utils/colorRandomizer.js
@@ -1,4 +1,15 @@
-const colors = ['#2a5a58b2', '#6a1b9ab2', '#33c1ffb2', '#36c95eb2', '#c9367db2', '#5c962fb2'];
+const colors = [
+    '#6366f1b2',
+    '#06b6d4b2',
+    '#10b981b2',
+    '#f59e0bb2',
+    '#ef4444b2',
+    '#8b5cf6b2',
+    '#ec4899b2',
+    '#3b82f6b2',
+    '#14b8a6b2',
+    '#f97316b2' 
+];
 
 function setRandomAccentColor() {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
